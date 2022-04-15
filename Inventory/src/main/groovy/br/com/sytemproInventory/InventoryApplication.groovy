45b68c924347a5573a1cb54ae7e1719fe548bec0
Fernandolilo <fernando.sistenpro@hotmail.com>
2022-04-15 11:01:19 -0300

    Iniciado projeto

---
+++ Inventory/src/main/groovy/br/com/sytemproInventory/InventoryApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.sytemproInventory
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InventoryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(InventoryApplication, args)
+	}
+
+}
---
+++ Inventory/src/test/groovy/br/com/sytemproInventory/InventoryApplicationTests.groovy
@@ -0,0 +1,13 @@
+package br.com.sytemproInventory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
